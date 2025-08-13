250090d87a9595780b1da9508f7513ca0ced5593
Rommexchile <ciper12@gmail.com>
2025-08-13 10:58:21 -0400

    Agregar ejemplo CRUD Flask para vehículos (persistencia en memoria)

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-import os
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rommex_fleet.db'
-app.config['UPLOAD_FOLDER'] = 'uploads'
-db = SQLAlchemy(app)
 
-from models import *
+# Simulación de base de datos en memoria
+vehiculos = []
 
 @app.route('/')
-def index():
-    vehicles = Vehicle.query.all()
-    return render_template('index.html', vehicles=vehicles)
+def home():
+    return redirect(url_for('listado_vehiculos'))
+
+@app.route('/vehiculos')
+def listado_vehiculos():
+    return render_template('listado.html', vehiculos=vehiculos)
+
+@app.route('/vehiculos/nuevo', methods=['GET', 'POST'])
+def agregar_vehiculo():
+    if request.method == 'POST':
+        nuevo = {
+            'id': vehiculos[-1]['id'] + 1 if vehiculos else 1,
+            'marca': request.form['marca'],
+            'modelo': request.form['modelo'],
+            'anio': request.form['anio']
+        }
+        vehiculos.append(nuevo)
+        return redirect(url_for('listado_vehiculos'))
+    return render_template('nuevo_vehiculo.html')
+
+@app.route('/vehiculos/editar/<int:id>', methods=['GET', 'POST'])
+def editar_vehiculo(id):
+    vehiculo = next((v for v in vehiculos if v['id'] == id), None)
+    if not vehiculo:
+        return 'Vehículo no encontrado', 404
+    if request.method == 'POST':
+        vehiculo['marca'] = request.form['marca']
+        vehiculo['modelo'] = request.form['modelo']
+        vehiculo['anio'] = request.form['anio']
+        return redirect(url_for('listado_vehiculos'))
+    return render_template('editar_vehiculo.html', vehiculo=vehiculo)
+
+@app.route('/vehiculos/eliminar/<int:id>', methods=['POST'])
+def eliminar_vehiculo(id):
+    global vehiculos
+    vehiculos = [v for v in vehiculos if v['id'] != id]
+    return redirect(url_for('listado_vehiculos'))
 
 if __name__ == '__main__':
-    if not os.path.exists('rommex_fleet.db'):
-        db.create_all()
     app.run(debug=True)
